01dccc249777250189efd5439f3a805566b51651
Raul Lima <79487007+raul-lima@users.noreply.github.com>
2021-09-27 14:43:26 -0300

    implementa o spring data JPA e cria as tabelas transacoes e usuarios

--- src/main/java/br/com/alura/carteira/modelo/Transacao.java
+++ src/main/java/br/com/alura/carteira/modelo/Transacao.java
@@ -2,18 +2,29 @@ package br.com.alura.carteira.modelo;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Getter @Setter @ToString @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "transacoes")
 public class Transacao {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
 
 	private String ticker;
 	private LocalDate data;
 	private BigDecimal preco;
-	private int quantidade;
+	private Integer quantidade;
+
+	@Enumerated(EnumType.STRING)
 	private TipoTransacao tipo;
+
+	@ManyToOne
+	private Usuario usuario;
 
 }
--- src/main/java/br/com/alura/carteira/modelo/Usuario.java
+++ src/main/java/br/com/alura/carteira/modelo/Usuario.java
@@ -2,11 +2,19 @@ package br.com.alura.carteira.modelo;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter @Setter
 @ToString(exclude = "senha")
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
     private String nome;
     private String login;
